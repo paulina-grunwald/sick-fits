8af8daa34c9c815cabe2b0335901c18e2b92a2cd
unknown <paulina.grunwald@gmail.com>
2019-03-17 07:35:49 +0100

    Add script which adds graphql yoga server

--- backend/src/createServer.js
+++ backend/src/createServer.js
@@ -1,12 +1,13 @@
 const { GraphQLServer } = require('graphql-yoga')
-const Mustation = require('./resolvers/Mutation')
+const Mutation = require('./resolvers/Mutation')
 const Query = require('./resolvers/Query')
 const db = require('./db')
 
-// Create the GraphQl Yoga Server
+// Create the GraphQL Yoga Server
+
 function createServer() {
   return new GraphQLServer({
-    typeDefts: './src/schema.graphql',
+    typeDefs: 'src/schema.graphql',
     resolvers: {
       Mutation,
       Query
@@ -14,7 +15,7 @@ function createServer() {
     resolverValidationOptions: {
       requireResolversForResolveType: false
     },
-    context: request => ({ ...request, db })
+    context: req => ({ ...req, db })
   })
 }
 
