2723e3dea28793fae6ccacf94cb776530a31f944
unknown <paulina.grunwald@gmail.com>
2019-03-17 17:02:25 +0100

    Add data model, mutations and queries.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const mutations = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // returns promise
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    )
+    return item
+  }
+}
 
-module.exports = mutations;
+module.exports = Mutation
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+const Query = {
+  async items(parent, args, ctx, info) {
+    console.log('test')
+    const items = await ctx.db.query.items()
+    return items
+  }
+}
 
-module.exports = Query;
+module.exports = Query
