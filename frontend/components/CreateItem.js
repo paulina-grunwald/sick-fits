c9c7f7ff93cade22cf89cbfd95cfe662bc972fbc
Paulina Grunwald <paulina.grunwald@gmail.com>
2019-03-31 16:53:56 +0200

    add posibility for user to upload image

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
-import { Mutation } from 'react-apollo'
-import gql from 'graphql-tag'
+import React, { Component } from "react"
+import { Mutation } from "react-apollo"
+import gql from "graphql-tag"
 import Router from "next/router"
-import Form from './styles/Form'
-import Error from './ErrorMessage'
-import formatMoney from '../lib/formatMoney'
+import Form from "./styles/Form"
+import Error from "./ErrorMessage"
+import formatMoney from "../lib/formatMoney"
 
 const CREATE_ITEM_MUTATION = gql`
 	mutation CREATE_ITEM_MUTATION(
@@ -38,8 +38,26 @@ class CreateItem extends Component {
 	onChange = e => {
 		const { name, type, value } = e.target
 		const val = type === "number" ? parseFloat(value) : value
-		this.setState({ [name]: val })
+    this.setState({ [name]: val })
 	}
+
+  uploadFile = async e => {
+    const files = e.target.files
+    const data = new FormData()
+    data.append("file", files[0])
+    data.append('upload_preset', 'ngndtnp7')
+    const result = await fetch
+      ("https://api.cloudinary.com/v1_1/paulinag/image/upload", {
+        method: "POST",
+        body: data
+      })
+    const file = await result.json()
+    console.log(file)
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    })
+  }
 
 	render() {
 		return (
@@ -48,47 +66,55 @@ class CreateItem extends Component {
 					<Form
 						onSubmit={async e => {
 							e.preventDefault()
-              const result = await createItem()
-              Router.push({
-                pathname: '/item',
-                query: {id: result.data.createItem.id}
-              })
+							const result = await createItem()
+							Router.push({
+								pathname: "/item",
+								query: { id: result.data.createItem.id }
+							})
 						}}
 					>
 						<Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
-							<label htmlFor='title'>
+              <label
+                htmlFor="file"
+                id="file"
+                name="file"
+                placeholder="Upload an image"
+                value={this.state.file}
+                onChange={this.onChange}
+              />
+              {this.state.image && <img width='200'src={this.state.image} alt='upload preview' />}
+							<label htmlFor="title">
 								Title
 								<input
-                  onChange={this.onChange}
-									type='text'
-									id='title'
-									name='title'
-									placeholder='Title'
+									onChange={this.uploadFile}
+									type="file"
+									id="file"
+									name="file"
+									placeholder="Upload an image"
 									required
-									value={this.state.title}
 								/>
 							</label>
-							<label htmlFor='price'>
+							<label htmlFor="price">
 								Price
 								<input
-                  onChange={this.onChange}
-									type='number'
-									id='price'
-									name='price'
-									placeholder='Price'
+									onChange={this.onChange}
+									type="number"
+									id="price"
+									name="price"
+									placeholder="Price"
 									required
 									value={this.state.price}
 								/>
 							</label>
-							<label htmlFor='description'>
+							<label htmlFor="description">
 								Description
 								<textarea
-                  onChange={this.onChange}
-									type='description'
-									id='description'
-									name='description'
-									placeholder='Enter a description'
+									onChange={this.onChange}
+									type="description"
+									id="description"
+									name="description"
+									placeholder="Enter a description"
 									required
 									value={this.state.description}
 								/>
