f72770c0ecf0982d56352944215d50eda1275d4e
Paulina Grunwald <paulina.grunwald@gmail.com>
2019-04-23 18:46:10 +0200

    Add functionality to be able to edit previously added item.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,24 +1,31 @@
 const Mutation = {
-  async createItem(parent, args, ctx, info) {
-    const item = await ctx.db.mutation.createItem({
-        data: {
-          ...args
-        }
-      },
-      info
-    )
-    return item
-  }
-  updateItem (parent, args, ctx, info) {
-    const updates = { ...args }
-    delete updates.id
-    return ctx.db.mutation.updateItem({
-      data: updates,
-      where: {
-        id: args.id
-      }
-    }, info)
-  }
+	async createItem(parent, args, ctx, info) {
+		const item = await ctx.db.mutation.createItem(
+			{
+				data: {
+					...args
+				}
+			},
+			info
+		)
+		return item
+	},
+	updateItem(parent, args, ctx, info) {
+		// first take a copy of the updates
+		const updates = { ...args }
+		// remove the ID from the updates
+		delete updates.id
+		// run the update method
+		return ctx.db.mutation.updateItem(
+			{
+				data: updates,
+				where: {
+					id: args.id
+				}
+			},
+			info
+		)
+	}
 }
 
 module.exports = Mutation
